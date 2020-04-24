dd0481e00bbe9a134e3279f2ef7923409e6a2676
lsalazar1 <lmsalazar78011@gmail.com>
2020-04-24 01:32:00 -0500

    Moving global values

---
+++ images.py
@@ -0,0 +1,130 @@
+# These are commonly used tuple values for RBG colors
+b = (0,0,255)
+w = (255,255, 255)
+y = (255, 255, 0)
+g = (0, 255, 0)
+lb = (173, 216, 230)
+bk = (0, 0, 0)
+gr = (134, 136, 138)
+
+# Lines 78-198 are RGB values for LED Matrix
+drizzle = [
+  bk, bk, bk, bk, bk, y, y, y,
+  bk, bk, w, w, w, w, y, y,
+  bk, w, w, w, w, w, w, y,
+  w, w, w, w, w, w, w, w,
+  lb, bk, lb, bk, lb, bk, lb, bk,
+  bk, lb, bk, lb, bk, lb, bk, lb,
+  lb, bk, lb, bk, lb, bk, lb, bk,
+  g, g, g, g, g, g, g, g,
+]
+
+drizzle2 = [
+  bk, bk, bk, bk, bk, y, y, y,
+  bk, bk, w, w, w, w, y, y,
+  bk, w, w, w, w, w, w, y,
+  w, w, w, w, w, w, w, w,
+  bk, lb, bk, lb, bk, lb, bk, lb,
+  lb, bk, lb, bk, lb, bk, lb, bk,
+  bk, lb, bk, lb, bk, lb, bk, lb,
+  g, g, g, g, g, g, g, g,
+]
+
+clear_skies = [
+  b, b, b, b, b, y, y, y,
+  b, b, b, b, b, y, y, y,
+  b, b, b, b, b, y, y, y,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  g, g, g, g, g, g, g, g
+]
+
+clear_skies2 = [
+  b, b, b, b, b, y, y, y,
+  w, w, b, b, b, y, y, y,
+  w, w, b, b, b, y, y, y,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  g, g, g, g, g, g, g, g
+]
+
+clear_skies3 = [
+  b, b, b, b, b, y, y, y,
+  b, b, w, w, b, y, y, y,
+  b, b, w, w, b, y, y, y,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  g, g, g, g, g, g, g, g
+]
+
+clear_skies4 = [
+  b, b, b, b, b, y, y, y,
+  b, b, b, b, w, w, y, y,
+  b, b, b, b, w, w, y, y,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  g, g, g, g, g, g, g, g
+]
+
+blizzard = [
+  gr, gr, gr, gr, gr, gr, gr, gr,
+  w, b, w, b, w, b, w, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  b, b, b, b, b, b, b, b,
+  g, g, g, g, g, g, g, g
+]
+
+blizzard2 = [
+  gr, gr, gr, gr, gr, gr, gr, gr,
+  b, w, b, w, b, w, b, w,
+  w, b, w, b, w, b, w, b,
+  b, w, b, w, b, w, b, w,
+  w, b, w, b, w, b, w, b,
+  b, w, b, w, b, w, b, w,
+  b, b, b, b, b, b, b, b,
+  w, g, g, w, g, g, w, w
+]
+
+blizzard3 =[
+  gr, gr, gr, gr, gr, gr, gr, gr,
+  w, b, w, b, w, b, w, b,
+  b, w, b, w, b, w, b, w,
+  w, b, w, b, w, b, w, b,
+  b, w, b, w, b, w, b, w,
+  w, b, w, b, w, b, w, b,
+  w, w, w, w, w, w, w, w,
+  w, w, w, w, w, w, w, w
+]
+
+face = [
+  b, b, b, b, b, b, b, b,
+  b, w, w, b, b, w, w, b,
+  b, w, w, b, b, w, w, b,
+  b, b, b, b, b, b, b, b,
+  w, w, b, b, b, b, w, w,
+  w, w, b, b, b, b, w, w,
+  b, b, w, w, w, w, b, b,
+  b, b, w, w, w, w, b, b
+]
+
+face2 = [
+  b, b, b, b, b, b, b, b,
+  b, w, w, b, b, w, w, b,
+  b, w, w, b, b, w, w, b,
+  b, b, b, b, b, b, b, b,
+  w, w, w, w, w, w, w, w,
+  w, w, w, w, w, w, w, w,
+  b, w, w, w, w, w, w, b,
+  b, b, w, w, w, w, b, b
+]
--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from sense_hat import SenseHat
 from time import sleep
+from images import *
 
 # Represents a sub-key of what data would look like if we used OpenWeather API
-data = [
-  {
-    "weather":{
-        "main": "Clear",
-        "description": "clear sky",
-        "temp": 78
-      }
-  },
-  {
-    "weather": {
-        "main": "Drizzle",
-        "description": "light drizzle",
-        "temp": 65
-      }
-  },
-  {
-    "weather":{
-        "main": "Blizzard",
-        "description": "heavy snow",
-        "temp": -24
-      }
-  }
-]
+# data = [
+#   {
+#     "weather":{
+#         "main": "Clear",
+#         "description": "clear sky",
+#         "temp": 78
+#       }
+#   },
+#   {
+#     "weather": {
+#         "main": "Drizzle",
+#         "description": "light drizzle",
+#         "temp": 65
+#       }
+#   },
+#   {
+#     "weather":{
+#         "main": "Blizzard",
+#         "description": "heavy snow",
+#         "temp": -24
+#       }
+#   }
+# ]
 
 
 def weather_image(x, y, z):
@@ -66,137 +67,6 @@ def weather_image(x, y, z):
   sense.show_message(y)
   sense.show_message(z)
 
-# These are commonly used tuple values for RBG colors
-b = (0,0,255)
-w = (255,255, 255)
-y = (255, 255, 0)
-g = (0, 255, 0)
-lb = (173, 216, 230)
-bk = (0, 0, 0)
-gr = (134, 136, 138)
-
-# Lines 78-198 are RGB values for LED Matrix
-drizzle = [
-  bk, bk, bk, bk, bk, y, y, y,
-  bk, bk, w, w, w, w, y, y,
-  bk, w, w, w, w, w, w, y,
-  w, w, w, w, w, w, w, w,
-  lb, bk, lb, bk, lb, bk, lb, bk,
-  bk, lb, bk, lb, bk, lb, bk, lb,
-  lb, bk, lb, bk, lb, bk, lb, bk,
-  g, g, g, g, g, g, g, g,
-]
-
-drizzle2 = [
-  bk, bk, bk, bk, bk, y, y, y,
-  bk, bk, w, w, w, w, y, y,
-  bk, w, w, w, w, w, w, y,
-  w, w, w, w, w, w, w, w,
-  bk, lb, bk, lb, bk, lb, bk, lb,
-  lb, bk, lb, bk, lb, bk, lb, bk,
-  bk, lb, bk, lb, bk, lb, bk, lb,
-  g, g, g, g, g, g, g, g,
-]
-
-clear_skies = [
-  b, b, b, b, b, y, y, y,
-  b, b, b, b, b, y, y, y,
-  b, b, b, b, b, y, y, y,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g
-]
-
-clear_skies2 = [
-  b, b, b, b, b, y, y, y,
-  w, w, b, b, b, y, y, y,
-  w, w, b, b, b, y, y, y,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g
-]
-
-clear_skies3 = [
-  b, b, b, b, b, y, y, y,
-  b, b, w, w, b, y, y, y,
-  b, b, w, w, b, y, y, y,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g
-]
-
-clear_skies4 = [
-  b, b, b, b, b, y, y, y,
-  b, b, b, b, w, w, y, y,
-  b, b, b, b, w, w, y, y,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g
-]
-
-blizzard = [
-  gr, gr, gr, gr, gr, gr, gr, gr,
-  w, b, w, b, w, b, w, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g
-]
-
-blizzard2 = [
-  gr, gr, gr, gr, gr, gr, gr, gr,
-  b, w, b, w, b, w, b, w,
-  w, b, w, b, w, b, w, b,
-  b, w, b, w, b, w, b, w,
-  w, b, w, b, w, b, w, b,
-  b, w, b, w, b, w, b, w,
-  b, b, b, b, b, b, b, b,
-  w, g, g, w, g, g, w, w
-]
-
-blizzard3 =[
-  gr, gr, gr, gr, gr, gr, gr, gr,
-  w, b, w, b, w, b, w, b,
-  b, w, b, w, b, w, b, w,
-  w, b, w, b, w, b, w, b,
-  b, w, b, w, b, w, b, w,
-  w, b, w, b, w, b, w, b,
-  w, w, w, w, w, w, w, w,
-  w, w, w, w, w, w, w, w
-]
-
-face = [
-  b, b, b, b, b, b, b, b,
-  b, w, w, b, b, w, w, b,
-  b, w, w, b, b, w, w, b,
-  b, b, b, b, b, b, b, b,
-  w, w, b, b, b, b, w, w,
-  w, w, b, b, b, b, w, w,
-  b, b, w, w, w, w, b, b,
-  b, b, w, w, w, w, b, b
-]
-
-face2 = [
-  b, b, b, b, b, b, b, b,
-  b, w, w, b, b, w, w, b,
-  b, w, w, b, b, w, w, b,
-  b, b, b, b, b, b, b, b,
-  w, w, w, w, w, w, w, w,
-  w, w, w, w, w, w, w, w,
-  b, w, w, w, w, w, w, b,
-  b, b, w, w, w, w, b, b
-]
-
 sense = SenseHat()
 sense.set_rotation(270)
 
@@ -228,11 +98,11 @@ while True:
         sense.show_message(msg)
         
       if event.direction == 'right':
-        x = 1
-        main = data[x]["weather"]["main"]
-        desc = data[x]["weather"]["description"]
-        temp = data[x]["weather"]["temp"]
-        weather_image(main, desc, temp)
+        # x = 1
+        # main = data[x]["weather"]["main"]
+        # desc = data[x]["weather"]["description"]
+        # temp = data[x]["weather"]["temp"]
+        # weather_image(main, desc, temp)
         
       if event.direction == "down":
         sense.show_message("Bye!")
