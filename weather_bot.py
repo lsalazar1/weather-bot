3baf0405df961c39f445c047f4967099d79a3ac1
lsalazar1 <lmsalazar78011@gmail.com>
2020-06-16 17:38:48 -0500

    Adding new methods

--- weather_bot.py
+++ weather_bot.py
@@ -16,7 +16,7 @@ class WeatherBot:
         self.name = input('Enter your name: ')
         self.city = input('Enter city (i.e Houston, Kansas City): ')
         self.state = input('Enter State Code (i.e TX): ')
-        
+
         self.sense.show_message(f'Hello, {self.name}!')
         self.sense.set_pixels(face)
         sleep(0.5)
@@ -29,7 +29,13 @@ class WeatherBot:
         self.sense.show_message('L - Show current weather')
         sleep(1)
         self.sense.show_message('D - Exit Program')
-        
+    
+    def show_currentWeather(self):
+        self.sense.show_message('Testing Current weather')
+
+    def end_program(self):
+        self.sense.show_message('Bye!')
+        exit()
 
 
         
