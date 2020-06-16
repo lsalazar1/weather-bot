b09008aae40820659df95af2d16474449a3da5e2
lsalazar1 <lmsalazar78011@gmail.com>
2020-06-16 17:39:34 -0500

    Implementing Weatherbot obj

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from weather_bot import WeatherBot
 
 if __name__ == '__main__':
   bot = WeatherBot()
+
+  try:
+    while True:
+      for event in bot.sense.get_events():
+        if event.action == 'pressed':
+          if event.direction == 'left':
+            bot.show_currentWeather()
+  except KeyboardInterrupt:
+    bot.end_program()
   
