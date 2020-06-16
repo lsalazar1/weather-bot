c2c14c165aae86ce80afa750e03e7ce543fa9bb4
lsalazar1 <lmsalazar78011@gmail.com>
2020-06-16 18:27:55 -0500

    Resolving error in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
   try:
     while True:
-      for event in bot.sense.get_events():
+      for event in bot.sense.stick.get_events():
         if event.action == 'pressed':
           if event.direction == 'left':
             bot.show_currentWeather()
